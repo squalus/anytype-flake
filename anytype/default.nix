eaa0e7c3691a672720997c8ece0bdf63819a739f
hellodword <46193371+hellodword@users.noreply.github.com>
2024-08-23 06:08:13 +0000

    anytype: v0.42.3 -> v0.42.4

--- anytype/default.nix
+++ anytype/default.nix
@@ -1,11 +1,11 @@
 { anytype-ts-src, anytype-l10n-src, anytype-heart, anytype-protos-js, fix-lockfile
 , remove-telemetry-deps
 , lib, fetchFromGitHub, fetchurl, makeWrapper, buildNpmPackage, fetchNpmDeps
-, pkg-config, libsecret, electron_30, libglvnd, jq, moreutils, stdenvNoCC }:
+, pkg-config, libsecret, electron_31, libglvnd, jq, moreutils, stdenvNoCC }:
 
 let
 
-  electron = electron_30;
+  electron = electron_31;
 
   pname = "anytype";
 
